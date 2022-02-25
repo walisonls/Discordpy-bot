9a9533eb525c5ad87accc9273622ded5b91fb862
Walison Lima <82228528+walisonls@users.noreply.github.com>
2022-02-25 18:02:36 +0000

    Atualização

--- main.py
+++ main.py
@@ -9,52 +9,39 @@ intents.members = True
 bot = commands.Bot(command_prefix="$", case_insensitive = True, intents = intents)
 point = 0
 
-## crate client ##
+## Bot Connected ##
 @bot.event
 async def on_ready():
-  print(f"Estou pronto! Estou conectado como {bot.user}")
-  current_time.start()
+  now = datetime.now()#importando relogio.
+  now = now.strftime(" %H:%M:%S de %d/%m/%Y")#configurando formato de horario.
+  channel = bot.get_channel(942944936771346443)#canal de comunicação
+  await channel.send(f"{bot.user} Conectada ao servidor as {now}")#menssagem
 
-## time response ##
-@tasks.loop(hours=24)
-async def current_time():
-  now = datetime.now()
-
-  now = now.strftime("%d/%m/%Y às %H:%M:%S")
-
-  channel = bot.get_channel(943122811747184720)
-
-  await channel.send("Ping Retornado, data: " + now)
-
-## message control ##
-@bot.event
-async def on_message(message):
-  if message.author == bot.user:
-    return
-
-  if "palavrão" in message.content:
-    await message.channel.send(f"Por Favor, {message.author.name}, não ofenda os demais usuarios!")
-
-    await message.delete()
-
-  await bot.process_commands(message) #processar comandos.
-
-  
-## response command ##
-@bot.command(name="oi")
-async def send_hello(ctx):
-  name = ctx.author.name
-  response = "Olá, " + name
-
-  await ctx.send(response)
 
 ## Seja Bem-Vindo(a) ##
 @bot.event
 async def on_member_join(member):
   canalboasvindas = bot.get_channel(942834048504561725)
-  regras = bot.get_channel(942844806084845659)
-  
-  message = await canalboasvindas.send(f"Bem vindo(a) {member.mention}! Leia as regras em ")
+  url_image="https://picsum.photos/1920/1080"
+
+  embed = discord.Embed(
+    title = "Resultado da busca de imagens",
+    description = "A busca é totalmente aleatoria",
+    color = 0x0000FF,
+  )
+
+  embed.set_author(name=bot.user.name, icon_url=bot.user.avatar_url)
+  embed.set_footer(text="ID do usuário " + bot.user.id)
+
+  embed.add_field(name="API", value="Usamos a API do https://picsum.photos")
+  embed.add_field(name="Parâmetros", value="{largura}/{altura}")
+
+  embed.add_field(name="Exemplo", value=url_image, inline="False")
+
+  embed.set_image(url=url_image)
+
+  ##await ctx.send(embed=embed_image)
+  message = await canalboasvindas.send(embed=embed)
   
 
 ## cargos por reação ##
@@ -67,28 +54,28 @@ async def on_reaction_add(reaction, user):
     role = user.guild.get_role(943859422252384298)
     await user.add_roles(role)
   
-## embed ##
+## Reaction Roles ##
 @bot.command(name="foto")
 async def get_random_image(ctx):
   url_image="https://picsum.photos/1920/1080"
 
-  embed_image = discord.Embed(
+  embed = discord.Embed(
     title = "Resultado da busca de imagens",
-    description = "A busca é totalmente aleatoria",
+    description = f"A busca é totalmente aleatoria",
     color = 0x0000FF,
   )
 
-  embed_image.set_author(name=bot.user.name, icon_url="bot.user.avatar_url")
-  embed_image.set_footer(text="Feito por " + bot.user.name, icon_url=bot.user.avatar_url)
+  embed.set_author(name=bot.user.name, icon_url=f"{bot.user.avatar_url}")
+  embed.set_footer(text=f"Feito por {bot.user.name}")
 
-  embed_image.add_field(name="API", value="Usamos a API do https://picsum.photos")
-  embed_image.add_field(name="Parâmetros", value="{largura}/{altura}")
+  embed.add_field(name="API", value="Usamos a API do https://picsum.photos")
+  embed.add_field(name="Parâmetros", value="{largura}/{altura}")
 
-  embed_image.add_field(name="Exemplo", value=url_image, inline="False")
+  embed.add_field(name="Exemplo", value=url_image, inline="False")
 
-  embed_image.set_image(url=url_image)
+  embed.set_image(url=url_image)
 
-  await ctx.send(embed=embed_image)
+  await ctx.send(embed=embed)
 
 
 ## recuperando TOKEN ##
